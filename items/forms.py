b22daa674cfba87af1a3aa3091d464a3328a815d
Jeremiah-Jefry <g.jeremiahjefry06@gmail.com>
2026-03-11 15:26:35 +0530

    feat: Introduce item reporting, detail, edit, delete, feed, and user login/registration pages and forms.

--- items/forms.py
+++ items/forms.py
@@ -4,7 +4,7 @@ from .models import Item
 class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
-        fields = ['title', 'status', 'description', 'location_description', 'image']
+        fields = ['title', 'status', 'category', 'description', 'location_description', 'image', 'handover_status', 'handover_details', 'receiver_name', 'receiver_contact']
         widgets = {
             'description': forms.Textarea(attrs={'rows': 4}),
             'location_description': forms.TextInput(attrs={'placeholder': 'E.g., Library 2nd Floor, Cafeteria...'}),
